5582e37b091d5a0a7f23fbd7cecc31efc47df54e
MostlyEmre <6238977+MostlyEmre@users.noreply.github.com>
2022-02-26 10:22:40 -0500

    Created the core app.

---
+++ main.js
@@ -0,0 +1,59 @@
+const handleFiles = (e) => {
+  const [file] = document.querySelector("input[type=file]").files;
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(reader.result, "text/xml");
+
+      const folders = xmlDoc.querySelectorAll("Folder");
+
+      let convertedData = [];
+
+      folders.forEach((folder) => {
+        let data = {
+          title: folder.firstElementChild.textContent,
+          points: [],
+        };
+
+        const placemarks = folder.querySelectorAll("Placemark");
+
+        placemarks.forEach((placemark) => {
+          let coordinates = placemark.querySelector("coordinates").textContent.split(",");
+
+          let point = {
+            text: placemark.firstElementChild.textContent, // title of the place
+            lat: coordinates[1].replace(/\s+/g, ""), // 47.6154276
+            lng: coordinates[0].replace(/\s+/g, ""), // -122.3497142
+          };
+
+          data.points.push(point);
+        });
+        convertedData.push(data);
+      });
+      content.innerHTML = JSON.stringify(convertedData, null, 2);
+      console.log(convertedData);
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsText(file);
+  }
+};
+const content = document.querySelector("#klmContent");
+const inputElement = document.getElementById("inputElement");
+inputElement.addEventListener("change", handleFiles, false);
+
+const copyBtn = document.querySelector("#copy");
+copyBtn.addEventListener("click", () => copyToClipboard(content.textContent));
+
+// write the array to the user's clipboard
+const copyToClipboard = async (string) => {
+  navigator.clipboard
+    .writeText(string)
+    .then(() => console.log("Copied! 😌"))
+    .catch((err) => console.log("Can't copy to clipboard.", err));
+};
